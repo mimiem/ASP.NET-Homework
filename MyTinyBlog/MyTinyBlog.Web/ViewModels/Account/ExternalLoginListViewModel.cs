49f8725b7384747a5ba41be5ee10de11a0d318e0
Maria Vardalieva <mimiem87@gmail.com>
2017-10-05 16:40:57 +0300

    refactoring the mvc models

---
+++ MyTinyBlog/MyTinyBlog.Web/ViewModels/Account/ExternalLoginListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTinyBlog.Web.ViewModels.Account
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ MyTinyBlog/MyTinyBlog.Web/ViewModels/Manage/ManageLoginsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTinyBlog.Web.ViewModels.Manage
+{
+    public class ManageLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+}
