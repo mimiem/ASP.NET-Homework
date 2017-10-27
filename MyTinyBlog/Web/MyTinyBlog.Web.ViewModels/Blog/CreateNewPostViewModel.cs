a7de4b99c3c446797e2c87bfdc8768cb6a0de5c3
Maria Vardalieva <mimiem87@gmail.com>
2017-10-28 00:10:22 +0300

    added data annotations in CreateNewPostViewModel.cs and EditPostViewModel.cs

--- MyTinyBlog/Web/MyTinyBlog.Web.ViewModels/Blog/CreateNewPostViewModel.cs
+++ MyTinyBlog/Web/MyTinyBlog.Web.ViewModels/Blog/CreateNewPostViewModel.cs
@@ -1,19 +1,28 @@
 ﻿namespace MyTinyBlog.Web.MyTinyBlog.Web.ViewModels.Blog
 {
+    using System.ComponentModel.DataAnnotations;
     using System.Web.Mvc;
 
     public class CreateNewPostViewModel
     {
+        [Required]
+        [MinLength(3,ErrorMessage = "{0} must be at least 3 symbols long")]
+        [MaxLength(30, ErrorMessage = "{0} must be max 30 symbols long")]
         public string Title { get; set; }
 
+        [Required]
         [AllowHtml]
         public string ShortContent { get; set; }
 
+
+        [Required]
         [AllowHtml]
         public string Content { get; set; }
 
+        [Required]
         public string Category { get; set; }
 
+        [Required]
         public string Tags { get; set; }
     }
 }
--- MyTinyBlog/Web/MyTinyBlog.Web.ViewModels/Blog/EditPostViewModel.cs
+++ MyTinyBlog/Web/MyTinyBlog.Web.ViewModels/Blog/EditPostViewModel.cs
@@ -1,21 +1,29 @@
-﻿using System.Web.Mvc;
-
-namespace MyTinyBlog.Web.MyTinyBlog.Web.ViewModels.Blog
+﻿namespace MyTinyBlog.Web.MyTinyBlog.Web.ViewModels.Blog
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
     public class EditPostViewModel
     {
         public int Id { get; set; }
 
+        [Required]
+        [MinLength(3, ErrorMessage = "{0} must be at least 3 symbols long")]
+        [MaxLength(30, ErrorMessage = "{0} must be max 30 symbols long")]
         public string Title { get; set; }
 
+        [Required]
         [AllowHtml]
         public string Content { get; set; }
 
+        [Required]
         [AllowHtml]
         public string ShortContent { get; set; }
 
+        [Required]
         public string Category { get; set; }
 
+        [Required]
         public string Tags { get; set; }
     }
 }
